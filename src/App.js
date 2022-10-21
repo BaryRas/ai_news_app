9e1f4dea37e4d96721e7148a0ac58354d54a91fd
“BaryRas” <gentimechant73@gmail.com>
2022-10-21 10:54:38 +0200

    feat: news card and AI search by sources and categories

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import React, { useEffect, useState } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
 import "./App.css";
+import NewsCards from "./components/NewsCards/NewsCards";
 
 function App() {
-  return <div className="App"></div>;
+  const [newsState, setNewsState] = useState([]);
+  useEffect(() => {
+    alanBtn({
+      key: process.env.REACT_APP_API_KEY,
+      onCommand: ({ command, data }) => {
+        if (command === "newHeadlines") {
+          setNewsState(data);
+          console.log(newsState, "app");
+        }
+      },
+    });
+  }, [newsState]);
+
+  return (
+    <div className="App">
+      <h1>AI NEWS APP</h1>
+      <NewsCards articles={newsState} />
+    </div>
+  );
 }
 
 export default App;
